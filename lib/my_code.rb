0f2c96b3bc61ec4c4edc605bc989d754179c1d82
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-08 02:25:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,4 +15,6 @@ end
 def reduce_to_total(source_array, starting_point=0)
   source_array.reduce(starting_point) { |total, num| total + num }
 end
-  
+def reduce_to_all_true(source_array)  
+  source_array.all?
+end
