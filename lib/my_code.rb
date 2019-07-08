4f61d6e799d870fcf3dfe95bab5456fa0810bf58
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-08 02:16:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,3 +6,6 @@ end
 def map_to_no_change(source_array) 
   dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
 end
+def map_to_double(source_array)
+  source_array.map { |source_array| source_array * 2 }
+end
